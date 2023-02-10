5b9fca6b1f9c7a9f22260509fd3601a1f225e420
TheGiantKiller <ktsdsd94@gmail.com>
2023-02-10 11:04:14 +0900

    feat : KAKAO 소셜로그인 서버 기능 구현 (Jwt 토큰 검증)

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,6 +1,7 @@
 const passport=require('passport')
 const express=require('express')
 const router = express.Router();
+const jwt=require('jsonwebtoken')
 
 
 router.get('/kakao',passport.authenticate('kakao'))
@@ -8,7 +9,15 @@ router.get('/kakao',passport.authenticate('kakao'))
 router.get('/kakao/callback',passport.authenticate('kakao',{
     failureRedirect:'/',
 }),(req,res)=>{
-    res.redirect('/music')
+    const payload={
+        email:req.user.email,
+        nickname:req.user.nick
+    }
+    const expire={expiresIn:'1h'}
+    const token=jwt.sign(payload,process.env.JWT_SECRET,expire)
+    res.json({
+        token:token
+    })
 })
 
 
