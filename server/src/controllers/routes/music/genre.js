2236c4b64931ffc61d4a600ddd29e6eeb4037292
TheGiantKiller <ktsdsd94@gmail.com>
2023-11-26 19:47:10 +0900

    feat : genre에 대한 음악 아이템 가져오는 API 수정

--- server/src/controllers/routes/music/genre.js
+++ server/src/controllers/routes/music/genre.js
@@ -12,12 +12,32 @@ const getGenreMusic = (req, res, next) => {
 
 const getGenreMusicById = (req, res, next) => {
   const genre_id = req.params.genre_id;
+  let { size, page } = req.query;
+
   let genreData = fs.readFileSync(path.join(process.cwd(), `src/files/music/${genre_id}.json`), "utf-8");
   genreData = JSON.parse(genreData);
+
+  size = Number(size); // 사용자가 요청한 size 양
+  page = Number(page); //사용자의 현재 page
+  const totalCount = genreData.music_list.length;
+  const totalPage = Math.round(totalCount / size);
+
+  const music_list = genreData.music_list.slice(page * size, (page + 1) * size);
+
   res.status(200).json({
-    status: "success",
-    result: genreData,
+    result: {
+      status: "success",
+      genre_title: genreData.genre_title,
+      genre_id: genreData.genre_id,
+      music_list,
+      page,
+      size,
+      totalPage,
+      isLastPage: totalPage <= page,
+      isFirstPage: page === 0,
+    },
   });
 };
+
 exports.getGenreMusic = getGenreMusic;
 exports.getMusicGenreById = getGenreMusicById;
