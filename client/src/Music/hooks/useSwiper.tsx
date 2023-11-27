354a8a422dcfeeb2037f9971230d59d27ab892e3
TheGiantKiller <ktsdsd94@gmail.com>
2023-11-27 19:21:22 +0900

    fix : Swiper 슬라이드시 장르음악 불러오지못하는 현상 해결

--- client/src/Music/hooks/useSwiper.tsx
+++ client/src/Music/hooks/useSwiper.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 import SwiperCore from 'swiper';
 import { Swiper } from 'swiper/react';
 import { SWIPER_SETTING } from 'Music/constants/swiper';
-import { getMusicList } from 'Music/store/feature/GenreMusicSlice';
+import { getFirstMusicList } from 'Music/store/feature/GenreMusicSlice';
 import { useAppDispatch } from 'shared/hooks/useReduxStore';
 
 const useSwiper = (slideitems) => {
@@ -13,17 +13,13 @@ const useSwiper = (slideitems) => {
     if (!swiperModule) return;
     swiperModule.slideNext(1000);
     const idx = swiperModule.realIndex + 1;
-
-    const req = { id: Number(idx), size: 11, page: 0 };
-    dispatch(getMusicList(req));
+    dispatch(getFirstMusicList(Number(idx)));
   }, [swiperModule]);
   const handlePrevSlide = useCallback(() => {
     if (!swiperModule) return;
     swiperModule.slidePrev(1000);
     const idx = swiperModule.realIndex + 1;
-    const req = { id: Number(idx), size: 11, page: 0 };
-
-    dispatch(getMusicList(req));
+    dispatch(getFirstMusicList(Number(idx)));
   }, [swiperModule]);
 
   const swiper = (
